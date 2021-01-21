ad36e6cd45e4ec81d608d3e52e124d2a0ae27d3f
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-01-21 15:12:13 +0100

    milestone 2 check

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 /*Milestone 1:
 - Definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da:
- nome, prefisso, tipo e famiglia.
+nome, prefisso, tipo e famiglia.
 - Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte
- le icone con il proprio nome.*/
+le icone con il proprio nome.*/
+
+/* Milestone 2:
+- Definire un array di colori e associare ad ogni tipo di icona un colore.
+- Visualizzare le icone di colore diverso in base al tipo. */
 
 const icons = [
   {name:'cat', prefix:'fa', type:'animal', family:'fas'},
@@ -21,15 +25,28 @@ const icons = [
   {name:'user-graduate', prefix:'fa', type:'user', family:'fas'},
   {name:'user-ninja', prefix:'fa', type:'user', family:'fas'},
   {name:'user-secret', prefix:'fa', type:'user', family:'fas'}
-]
+];
+
+const colors = ['yellow', 'red', 'blue'];
+const [forAnimals, forVegs, forUsers] = colors;
 
 const appContainer = document.getElementById('container');
 
 icons.forEach((item) => {
-  const {name, prefix, family} = item;
+  const {name, prefix, family, type} = item;
+  let colorIcon;
+  if (type === 'animal') {
+    colorIcon = forAnimals;
+  }
+  if (type === 'vegetable') {
+    colorIcon = forVegs;
+  }
+  if (type === 'user') {
+    colorIcon = forUsers;
+  }
   appContainer.innerHTML += `
   <div class="inner">
-  <i class="${family} ${prefix}-${name}"></i>
+  <i class="${family} ${prefix}-${name}" style="color:${colorIcon}"></i>
   </div>
   `
 });
